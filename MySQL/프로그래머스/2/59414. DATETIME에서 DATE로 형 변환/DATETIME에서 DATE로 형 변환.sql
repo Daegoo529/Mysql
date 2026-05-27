61d70de115c0c3bbaf3b59de968f133bbcb8f908
Daegoo529 <daegoo123@gmail.com>
2026-05-27 16:46:03 +0900

    Update DATETIME에서 DATE로 형 변환.sql

--- MySQL/프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
+++ MySQL/프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -1,4 +1,6 @@
 -- 코드를 입력하세요
 SELECT animal_id,name, date_format(datetime,'%Y-%m-%d') 날짜
+-- date_format(datetime,'%Y-%m-%d') 대신에 
+-- substring(datetime,1,10) 을 써도 이 문제는 해결 가능
 from animal_ins
 order by animal_id
