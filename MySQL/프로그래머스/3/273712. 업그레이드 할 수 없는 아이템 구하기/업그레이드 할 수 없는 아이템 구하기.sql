cf0b096cc8431f55cf25f06592a4bcde4634f204
Daegoo529 <daegoo123@gmail.com>
2026-06-05 09:16:20 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 작성해주세요
+SELECT  i.ITEM_ID, i.ITEM_NAME, i.RARITY
+FROM ITEM_INFO i left join ITEM_TREE t
+on i.item_id = t.parent_item_id
+where t.parent_item_id is null
+order by i.item_id desc
